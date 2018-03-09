278a305c4eb12c9db5331eb81696f2378270bb61
Lenz Weber <mail@lenzw.de>
2018-03-09 21:09:20 +0100

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,25 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "babel-preset-env",
+                {
+                  targets: {
+                    browsers: ["last 2 versions", "safari >= 7"]
+                  }
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
